c430770b9157a1c1bc2a94acfb9cd016cb20fa6e
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-07-11 12:25:03 -0300

    Disabling apollo client caching It was causing gliches on video loading

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,19 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, DefaultOptions, InMemoryCache } from "@apollo/client";
+
+const defaultOptions: DefaultOptions = {
+    watchQuery: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'ignore',
+    },
+    query: {
+      fetchPolicy: 'network-only',
+      errorPolicy: 'all',
+    },
+  }
+
 
 export const client = new ApolloClient({
     uri: 'https://api-sa-east-1.graphcms.com/v2/cl4qz2d1j0cep01z34ruy2dcw/master',
-    cache: new InMemoryCache()
+    cache: new InMemoryCache(),
+    defaultOptions: defaultOptions
 })
