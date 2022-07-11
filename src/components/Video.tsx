031104576e0d6bc1f4598fd9240aaf5f6f317ad6
Eliezer Marques Mafra <32603174+EliezerMafra@users.noreply.github.com>
2022-07-11 12:04:16 -0300

    Concluding routing

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,64 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+import { CaretRight, CircleNotch, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css'
 
-export function Video(){
-    return(
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+    query GetLessonBySlug($slug: String) {
+        lesson(where: {slug: $slug}) {
+            title
+            videoId
+            description
+            teacher {
+                avatarURL
+                bio
+                name
+            }
+        }
+    }
+`
+
+interface GetLessonBySlugResponse{
+    lesson:{
+        title: string;
+        videoId: string;
+        description: string;
+        teacher: {
+            avatarURL: string;
+            bio: string;
+            name: string;
+        }
+    }
+}
+
+interface VideoProps {
+    lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+
+    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+        variables: {
+            slug: props.lessonSlug
+        }
+    })
+
+    if (!data) {
+        return(
+            <div className="flex-1 flex items-center justify-center">
+                <CircleNotch size={50} />
+            </div>
+        )
+    }
+
+    return (
         <div className="flex-1">
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video ">
                     <Player>
-                        <Youtube videoId="VnT7pT6zCcA"/>
+                        <Youtube videoId={data.lesson.videoId}/>
                         <DefaultUi />
                     </Player>
                 </div>
@@ -18,35 +67,35 @@ export function Video(){
                 <div className="flex items-start gap-16">
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold ">
-                            Aula 01
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Loren Ipsun
+                            {data.lesson.description}
                         </p>
 
                         <div className="flex items-center gap-4 mt-6">
-                            <img 
+                            <img
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="http://github.com/EliezerMafra.png" 
-                                alt="" 
+                                src={data.lesson.teacher.avatarURL}
+                                alt=""
                             />
 
                             <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">Eliezer Mafra</strong>
-                                <span className="text-gray-200 text-sm block">Automation Engineer</span>
+                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
                             </div>
                         </div>
                     </div>
 
 
                     <div className="flex flex-col gap-4">
-                        <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors "> 
-                            <DiscordLogo size={24}/>
+                        <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors ">
+                            <DiscordLogo size={24} />
                             Comunidade Discord
                         </a>
 
-                        <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transiton-colors"> 
-                            <Lightning size={24}/>
+                        <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transiton-colors">
+                            <Lightning size={24} />
                             Acesse o desafio
                         </a>
                     </div>
@@ -56,7 +105,7 @@ export function Video(){
                 <div className="gap-8 mt-20 grid grid-cols-2">
                     <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
-                            <FileArrowDown size={40}/>
+                            <FileArrowDown size={40} />
                         </div>
                         <div className="py-6 leading-relaxed">
                             <strong className="text-2xl">Material Complementar</strong>
@@ -65,12 +114,12 @@ export function Video(){
                             </p>
                         </div>
                         <div className="h-full p-6 flex items-center">
-                            <CaretRight size={24}/>
+                            <CaretRight size={24} />
                         </div>
                     </a>
                     <a href="#" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
                         <div className="bg-green-700 h-full p-6 flex items-center">
-                            <Image size={40}/>
+                            <Image size={40} />
                         </div>
                         <div className="py-6 leading-relaxed">
                             <strong className="text-2xl">Wallpapers exclusivos</strong>
@@ -79,7 +128,7 @@ export function Video(){
                             </p>
                         </div>
                         <div className="h-full p-6 flex items-center">
-                            <CaretRight size={24}/>
+                            <CaretRight size={24} />
                         </div>
                     </a>
                 </div>
